b0da5a8a81933d8f629d70aa95b0c4923c88af26
Zach van Noppen <44442526+ZachvanNoppen@users.noreply.github.com>
2019-12-29 20:02:18 -0500

    Starting dev on the site

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
 <?php
-header("Location: index.html");
+
+/* the following is the conenction info for the // DB
+
+$conn = pg_connect(getenv("DATABASE_URL"));
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    echo 'Initial Connect Failed';
+}
+$name =$_POST["name"];
+
+$sql = "UPDATE testnames SET name='$name' WHERE pid=0";
+
+if (!pg_query($conn,$sql)) {
+  echo "Error updating record: ";
+
+} else {
+    echo "Record updated successfully";
+}
+
+*/
+
+header("Location: submit.html");
 exit;
 ?>
